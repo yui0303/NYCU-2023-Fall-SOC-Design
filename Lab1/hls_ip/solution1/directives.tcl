19ba77a667da9b3f4edc9db372e4b1434eeb5e6a
yui0303 <iampeter62@gmail.com>
2023-09-30 03:47:31 +0800

    Add vitis_hls IP files

---
+++ Lab1/hls_ip/solution1/directives.tcl
@@ -0,0 +1,9 @@
+############################################################
+## This file is generated automatically by Vitis HLS.
+## Please DO NOT edit it.
+## Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+############################################################
+set_directive_top -name multip_2num "multip_2num"
+set_directive_interface -mode s_axilite "multip_2num" n32In1
+set_directive_interface -mode s_axilite "multip_2num" n32In2
+set_directive_interface -mode s_axilite "multip_2num" pn32ResOut
